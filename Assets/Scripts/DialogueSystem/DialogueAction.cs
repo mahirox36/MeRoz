e2aadff403bf43edd6112edc4b5a2cc7f0f59c5d
mahirox36 <ragontube1@gmail.com>
2025-01-03 03:31:45 +0300

    Updated the Actions

--- Assets/Scripts/DialogueSystem/DialogueAction.cs
+++ Assets/Scripts/DialogueSystem/DialogueAction.cs
@@ -1,13 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 [System.Serializable]
 public class DialogueAction
 {
     public ActionType actionType;
     public string parameter;
+    public Sprite image;
+    public AudioClip audio;
+    public CharacterData character;
+    public AnimationType characterAnimation;
 }
 
 public enum ActionType
 {
     ChangeBackground,
-    ChangeSprite,
+    ChangeEmotion,
+    CreateCharacter,
+    DeleteCharacter,
     PlaySound
+}
+
+public enum AnimationType
+{
+    None,
+    FadeIn,
+    SlideIn,
+    FadeOut,
+    SlideOut
 }
